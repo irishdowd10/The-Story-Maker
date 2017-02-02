af03ae06c8fef80eee551e222bfc19d3fb88804f
John Dowd <irishdowd10@gmail.com>
2017-02-02 15:12:08 -0800

    Updated story css folder

--- README.md
+++
@@ -1,2 +0,0 @@
-# The-Story-Maker
- A nice little story
--- story.js
+++
@@ -1,8 +0,0 @@
-var adjective = prompt('Please type and adjective')
-var sentence = "<h2> There once was a  " + adjective;
-var verb = prompt('Please type a verb')
-var noun = prompt('Please type a noun')
-alert( "Done??  Are you ready for some fun" );
-sentence +=' crane inspector that liked to use OSHA Peridic compliance inspections to '+ verb;
-sentence +=' the ' + noun + '.</h2>';
-document.write(sentence);
